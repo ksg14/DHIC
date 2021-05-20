918b569b06e432bc33e8b6d630876ffb427d1518
Karanjit Gill <ksg207@live.com>
2021-05-20 16:48:48 +0530

    :ambulance: Changed has_english_char
    Using regex instead of isalpha

--- utils/check_captions.py
+++ utils/check_captions.py
@@ -1,6 +1,7 @@
 import json
 from typing import List
 from tqdm import tqdm
+import re
 
 from config import Config
 
@@ -16,8 +17,10 @@ from indicnlp.tokenize import sentence_tokenize
 from indicnlp.tokenize import indic_tokenize  
 
 def has_english_char (text : str) -> bool:
-	for char in text:			
-		if char.isalpha ():
+	reg = re.compile(r'[a-zA-Z]')		
+	for char in text:	
+		if reg.match(char):
+			# print (f'char - {char}')
 			return True
 	return False
 
@@ -26,11 +29,21 @@ def clean_captions (captions : List) -> List:
 	for img in tqdm (captions):
 		sentences=sentence_tokenize.sentence_split(img ['caption'], lang='hi')
 
+		# print (f'raw - {sentences} , len - {len (sentences)}')
+
 		uniq_sentences = set (sentences)
+		
+		# print (f'uniq - {uniq_sentences}  , len - {len (uniq_sentences)}')
+
 		final_sentences = []
 		for sent in uniq_sentences:
+			# print (f'sent - {sent}')
 			if not has_english_char (sent):
+				# print ('Added')
 				final_sentences.append (sent)
+			# else:
+			# 	print ('not added')
+
 		clean_captions.append ({
 			'image_id' : img ['image_id'],
 			'caption' : ' '.join (final_sentences)
