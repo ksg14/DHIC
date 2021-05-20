b89f74b275486a870cc6b13895a675d2803e3b57
Karanjit Gill <ksg207@live.com>
2021-05-20 21:23:15 +0530

    :construction: train file added
    train code with dataloader added

--- model/model.py
+++ model/model.py
@@ -3,14 +3,13 @@ from torch import nn
 from model_utils import PatchEmbedding
 from encoder import TransformerEncoder
 
-class ViT(nn.Sequential):
+class VisionTransformerEncoder(nn.Sequential):
     def __init__(self,     
                 in_channels: int = 3,
                 patch_size: int = 16,
                 emb_size: int = 768,
                 img_size: int = 224,
                 depth: int = 12,
-                n_classes: int = 1000,
                 **kwargs):
         super().__init__(
             PatchEmbedding(in_channels, patch_size, emb_size, img_size),
---
+++ train.py
@@ -0,0 +1,31 @@
+# Indic library
+import sys
+from indicnlp import common
+INDIC_NLP_LIB_HOME=r"indic_nlp_library"
+INDIC_NLP_RESOURCES=r"indic_nlp_resources"
+sys.path.append(r'{}\src'.format(INDIC_NLP_LIB_HOME))
+common.set_resources_path(INDIC_NLP_RESOURCES)
+
+from indicnlp.tokenize import indic_tokenize  
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+from utils.dataset import HVGDataset
+
+from utils.config import Config
+
+if __name__ == '__main__':
+	config = Config ()
+
+	train_dataset = HVGDataset (config.train_captions, config.word_to_index_path, config.index_to_word_path, config.images_path, text_transform=indic_tokenize.trivial_tokenize)
+	train_dataloader = DataLoader (train_dataset, batch_size=1, shuffle=False)
+
+	for _, (image, caption, target, target_seq_len) in enumerate (train_dataloader):
+		print (f'image - {image}.shape')
+		print (f'caption - {caption}.shape')
+		print (f'target - {target}.shape')
+		print (f'target_seq_len - {target_seq_len}.shape')
+
+		break
+	
