b5cedc21b212c61f9a621eb05fc1730772199bf5
Karanjit Gill <ksg207@live.com>
2021-06-10 19:59:59 +0530

    :coffin: removed pytorch vit utils code

--- model/model_utils.py
+++ model/model_utils.py
@@ -4,8 +4,8 @@ import torch.nn.functional as F
 from torch import nn
 from torch import Tensor
 from torchvision.transforms import Compose, Resize, ToTensor
-from einops import rearrange, reduce, repeat
-from einops.layers.torch import Rearrange, Reduce
+# from einops import rearrange, reduce, repeat
+# from einops.layers.torch import Rearrange, Reduce
 
 import math
 
@@ -26,57 +26,57 @@ class PositionalEncoding(nn.Module):
         x = x + self.pe[:x.size(0), :]
         return self.dropout(x)
 
-class PatchEmbedding(nn.Module):
-    def __init__(self, in_channels: int = 3, patch_size: int = 16, emb_size: int = 768, img_size: int = 224):
-        self.patch_size = patch_size
-        super().__init__()
-        self.projection = nn.Sequential(
-            # using a conv layer instead of a linear one -> performance gains
-            nn.Conv2d(in_channels, emb_size, kernel_size=patch_size, stride=patch_size),
-            Rearrange('b e (h) (w) -> b (h w) e'),
-        )
-        self.cls_token = nn.Parameter(torch.randn(1,1, emb_size))
-        self.positions = nn.Parameter(torch.randn((img_size // patch_size) **2 + 1, emb_size))
+# class PatchEmbedding(nn.Module):
+#     def __init__(self, in_channels: int = 3, patch_size: int = 16, emb_size: int = 768, img_size: int = 224):
+#         self.patch_size = patch_size
+#         super().__init__()
+#         self.projection = nn.Sequential(
+#             # using a conv layer instead of a linear one -> performance gains
+#             nn.Conv2d(in_channels, emb_size, kernel_size=patch_size, stride=patch_size),
+#             Rearrange('b e (h) (w) -> b (h w) e'),
+#         )
+#         self.cls_token = nn.Parameter(torch.randn(1,1, emb_size))
+#         self.positions = nn.Parameter(torch.randn((img_size // patch_size) **2 + 1, emb_size))
 
         
-    def forward(self, x: Tensor) -> Tensor:
-        b, _, _, _ = x.shape
-        x = self.projection(x)
-        cls_tokens = repeat(self.cls_token, '() n e -> b n e', b=b)
-        # prepend the cls token to the input
-        x = torch.cat([cls_tokens, x], dim=1)
-        # add position embedding
-        x += self.positions
-        return x
+#     def forward(self, x: Tensor) -> Tensor:
+#         b, _, _, _ = x.shape
+#         x = self.projection(x)
+#         cls_tokens = repeat(self.cls_token, '() n e -> b n e', b=b)
+#         # prepend the cls token to the input
+#         x = torch.cat([cls_tokens, x], dim=1)
+#         # add position embedding
+#         x += self.positions
+#         return x
 
-class MultiHeadAttention(nn.Module):
-    def __init__(self, emb_size: int = 768, num_heads: int = 8, dropout: float = 0):
-        super().__init__()
-        self.emb_size = emb_size
-        self.num_heads = num_heads
-        # fuse the queries, keys and values in one matrix
-        self.qkv = nn.Linear(emb_size, emb_size * 3)
-        self.att_drop = nn.Dropout(dropout)
-        self.projection = nn.Linear(emb_size, emb_size)
+# class MultiHeadAttention(nn.Module):
+#     def __init__(self, emb_size: int = 768, num_heads: int = 8, dropout: float = 0):
+#         super().__init__()
+#         self.emb_size = emb_size
+#         self.num_heads = num_heads
+#         # fuse the queries, keys and values in one matrix
+#         self.qkv = nn.Linear(emb_size, emb_size * 3)
+#         self.att_drop = nn.Dropout(dropout)
+#         self.projection = nn.Linear(emb_size, emb_size)
         
-    def forward(self, x : Tensor, mask: Tensor = None) -> Tensor:
-        # split keys, queries and values in num_heads
-        qkv = rearrange(self.qkv(x), "b n (h d qkv) -> (qkv) b h n d", h=self.num_heads, qkv=3)
-        queries, keys, values = qkv[0], qkv[1], qkv[2]
-        # sum up over the last axis
-        energy = torch.einsum('bhqd, bhkd -> bhqk', queries, keys) # batch, num_heads, query_len, key_len
-        if mask is not None:
-            fill_value = torch.finfo(torch.float32).min
-            energy.mask_fill(~mask, fill_value)
+#     def forward(self, x : Tensor, mask: Tensor = None) -> Tensor:
+#         # split keys, queries and values in num_heads
+#         qkv = rearrange(self.qkv(x), "b n (h d qkv) -> (qkv) b h n d", h=self.num_heads, qkv=3)
+#         queries, keys, values = qkv[0], qkv[1], qkv[2]
+#         # sum up over the last axis
+#         energy = torch.einsum('bhqd, bhkd -> bhqk', queries, keys) # batch, num_heads, query_len, key_len
+#         if mask is not None:
+#             fill_value = torch.finfo(torch.float32).min
+#             energy.mask_fill(~mask, fill_value)
             
-        scaling = self.emb_size ** (1/2)
-        att = F.softmax(energy, dim=-1) / scaling
-        att = self.att_drop(att)
-        # sum up over the third axis
-        out = torch.einsum('bhal, bhlv -> bhav ', att, values)
-        out = rearrange(out, "b h n d -> b n (h d)")
-        out = self.projection(out)
-        return out
+#         scaling = self.emb_size ** (1/2)
+#         att = F.softmax(energy, dim=-1) / scaling
+#         att = self.att_drop(att)
+#         # sum up over the third axis
+#         out = torch.einsum('bhal, bhlv -> bhav ', att, values)
+#         out = rearrange(out, "b h n d -> b n (h d)")
+#         out = self.projection(out)
+#         return out
 
 
 class ResidualAdd(nn.Module):
