2a00f887ea3a6b58ae6ca8a305e1e2d17b1c57ad
Karanjit Gill <ksg207@live.com>
2021-06-12 21:04:50 +0530

    :monocle_face: added logs

--- utils/dataset.py
+++ utils/dataset.py
@@ -54,17 +54,18 @@ class HVGDataset (Dataset):
 			seq_len = len (caption_tok)
 			print (f'seq len - {seq_len}')
 
-			caption = self.decoder_transform (caption_tok, is_split_into_words=True, return_tensors='pt')
-			tok_len = caption.input_ids.shape [1]
+			tmp_caption = self.decoder_transform (caption_tok, is_split_into_words=True, return_tensors='pt')
+			tok_len = tmp_caption.input_ids.shape [1]
 
-			print (f'caption ids - {caption.input_ids.shape}')
-			print (f'caption ids - {caption.input_ids}')
-			print (f'decode - {self.decoder_transform.decode (caption.input_ids [0])}')
+			# print (f'caption ids - {caption.input_ids.shape}')
+			# print (f'caption ids - {caption.input_ids}')
+			# print (f'decode - {self.decoder_transform.decode (caption.input_ids [0])}')
 			print (f'tok len - {tok_len}')
 
 			caption = self.decoder_transform (caption_tok, is_split_into_words=True, max_length=self.max_len, padding='max_length', return_attention_mask=True, return_tensors='pt')
-		
-			target = self.decoder_transform (caption_tok [1:], is_split_into_words=True, max_length=self.max_len, padding='max_length', return_attention_mask=True, return_tensors='pt')
+
+			print (f'ids - {caption.input_ids}')
+			print (f'mask - {caption.attention_mask}')
 
 			return image, caption.input_ids, caption.attention_mask, target.input_ids, target.attention_mask	
 
